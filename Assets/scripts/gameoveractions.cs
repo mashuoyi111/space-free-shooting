26641a7dbac6e766dc7ed2d270cda6f9917fd67f
Max Ma <mashuoyi111@sina.com>
2017-06-01 22:36:47 -0700

    gameover restart

---
+++ Assets/scripts/gameoveractions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameoveractions : MonoBehaviour {
+
+    public void restart(){
+        SceneManager.LoadScene("mainGame");
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
